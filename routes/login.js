5d4e7cb00789745df3cf51fa3fcae866636c32fd
marvic <martinhuacho@hotmail.com>
2020-01-22 21:43:28 -0500

    agregando refresh token

--- routes/login.js
+++ routes/login.js
@@ -11,6 +11,23 @@ var CLIENT_ID = require('../config/config').CLIENT_ID;
 const {OAuth2Client} = require('google-auth-library');
 const client = new OAuth2Client(CLIENT_ID);
 
+var mdAutenticacion = require('../middlewares/autentication');
+
+// ===========================================
+// rfresh token
+// ===========================================
+app.get('/refreshToken', mdAutenticacion.verificaToken, (req, res) => {
+
+    var token = jwt.sign({ usuario: req.usuario }, SEED,{ expiresIn: 14400 })
+
+    return res.status(200).json({
+        ok: true,
+        token: token
+    })
+})
+
+
+
 // ===========================================
 // Autenticacion de google
 // ===========================================
